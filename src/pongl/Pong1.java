78d61e7b97a254c3f56d091a42291cb262e37c5a
Sebbx <56113951+Sebbx@users.noreply.github.com>
2021-05-14 13:21:13 +0200

    7. Przekształcenie programu na wersję z wieloma kulami. Program po przepisaniu nie działał bez wywołania funkcji InitKula (co zrobiłem w metodzie run).

--- src/pongl/Pong1.java
+++ src/pongl/Pong1.java
@@ -32,50 +32,58 @@ public class Pong1<setCycleCount> extends Application{
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        Timeline t = new Timeline(new KeyFrame(Duration.millis(15), e -> run(gc)));
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
         t.setCycleCount(Timeline.INDEFINITE);
 
         stage.setTitle("Kulki!");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
-//        gc.setFill(Color.BLACK);
-//        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDHT, ARENAHEIGHT);
-//        gc.setFill(Color.WHITESMOKE);
-//        gc.fillOval(ARENAX1+ARENAWIDHT/2, ARENAY1+ARENAHEIGHT/2,2*R ,2*R);
-
-
         t.play();
-
     }
 
-    private double x = ARENAX1+ARENAWIDHT/2;
-    private double y = ARENAY1+ARENAHEIGHT/2;
+//    private double x = ARENAX1+ARENAWIDHT/2;
+//    private double y = ARENAY1+ARENAHEIGHT/2;
+//
+//    private double vx = 5;
+//    private double vy = 2;
 
-    private double vx = 5;
-    private double vy = 2;
+    private static final int LICZBAKULEK = 10;
+    private double[] x = new double[LICZBAKULEK];
+    private double[] y = new double[LICZBAKULEK];
+    private double[] vx = new double[LICZBAKULEK];
+    private double[] vy = new double[LICZBAKULEK];
 
     private void run(GraphicsContext gc)
     {
+        initKula();
         gc.setFill(Color.BLACK);
         gc.fillRect(ARENAX1, ARENAY1, ARENAWIDHT, ARENAHEIGHT);
 
-        if ((x - R <= ARENAX1) || ((x + R >= ARENAX2))) vx = -vx;
-        if ((y - R <= ARENAY1) || ((y + R >= ARENAY2))) vy = -vy;
+        for (int i =0; i< LICZBAKULEK; i++) {
+            if ((x[i] - R <= ARENAX1) || ((x[i] + R >= ARENAX2))) vx[i] = -vx[i];
+            if ((y[i] - R <= ARENAY1) || ((y[i] + R >= ARENAY2))) vy[i] = -vy[i];
+        }
 
-        x += vx;
-        y += vy;
+        for (int i =0; i< LICZBAKULEK; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
 
-        gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(x-R, y-R, 2*R,2*R);
+        for (int i =0; i< LICZBAKULEK; i++) {
+            gc.setFill(Color.WHITESMOKE);
+            gc.fillOval(x[i] - R, y[i] - R, 2 * R, 2 * R);
+        }
     }
 
     private void initKula(){
         Random lott = new Random ();
-        x = lott.nextDouble() * ARENAWIDHT+ARENAX1;
-        y = lott.nextDouble() * ARENAHEIGHT+ARENAY1;
-        vx = 5+lott.nextDouble() * 20;
-        vy = 5+lott.nextDouble() * 20;
+        for (int i =0; i< LICZBAKULEK; i++) {
+            x[i] = lott.nextDouble() * ARENAWIDHT + ARENAX1;
+            y[i] = lott.nextDouble() * ARENAHEIGHT + ARENAY1;
+            vx[i] = 5 + lott.nextDouble() * 20;
+            vy[i] = 5 + lott.nextDouble() * 20;
+        }
     }
 
 
